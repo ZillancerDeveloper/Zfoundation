d28e7d4a576282bb7f80bfcb9bb9b607e233f9f6
Farzana <farzana09bd@gmail.com>
2023-07-10 13:56:24 +0600

    Constance config effect in django amdin dynamically

--- foundation/signals.py
+++ foundation/signals.py
@@ -1,9 +1,21 @@
 from allauth.socialaccount.models import SocialAccount
 from django.db.models.signals import post_save
+from django.conf import settings
 from django.dispatch import receiver
 
 from foundation.models import User
 from foundation.utils.notifications import welcome_email_notification
+from constance.signals import config_updated
+import os
+
+
+@receiver(config_updated)
+def constance_updated(sender, key, old_value, new_value, **kwargs):
+    if key == "LOGO_IMAGE":
+        file_path = f"{settings.MEDIA_ROOT}\{old_value}"
+
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
 
 # call function when click on save social account
