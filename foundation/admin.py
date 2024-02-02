8a10c00c0203828bf704190e4e193dbcbcaf370d
Ahmed Nabih <ahmed.osama99@live.com>
2024-02-02 10:07:33 +0200

    roll back

--- foundation/admin.py
+++ foundation/admin.py
@@ -92,7 +92,7 @@ class CustomUserAdmin(UserAdmin):
     search_fields = [
         "email",
     ]
-    list_display = ["email", "name", "is_active", "is_staff", "user_type"]
+    list_display = ["email", "name", "is_active", "is_staff", "user_type"] #,language
     list_display_links = ["email", "is_active", "is_staff"]
     list_filter = ("is_active", "is_staff", "is_superuser")
     ordering = ("email",)
